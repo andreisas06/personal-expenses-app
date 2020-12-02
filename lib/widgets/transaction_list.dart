aea8c9ac6369213e73bfa47bff867284ad846537
Andrei Sas <andreisas007@gmail.com>
2020-12-02 10:03:39 +0100

    minor improvements

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
+      height: MediaQuery.of(context).,
       child: transactions.isEmpty
           ? Column(
               children: [

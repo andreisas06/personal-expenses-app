5fc8bb7f6462b146969ae86916a71e69e27965b7
Andrei Sas <andreisas007@gmail.com>
2020-12-06 12:00:32 +0100

    improvments

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import './widgets/chart.dart';
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
 import './models/transaction.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
